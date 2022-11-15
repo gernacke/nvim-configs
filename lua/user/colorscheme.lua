95e1e133087479a3c5a770d7c4736a3616c42660
gernacke <gernacke@hotmail.com>
2022-11-15 20:40:11 +1100

    enabled comments in italic

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,6 +1,9 @@
 local colorscheme = "gruvbox"
+local cmd = vim.cmd
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
   return
 end
+
+cmd [[ highlight Comment cterm=italic gui=italic ]]
