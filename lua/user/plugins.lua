5ebd5e32add89ce7decfa43720515e7703131f2d
gernacke <gernacke@hotmail.com>
2023-04-16 18:02:24 +1000

    setup zk using telescope for searching notes

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -151,7 +151,27 @@ function M.setup()
         use({
             "mickael-menu/zk-nvim",
             config = function()
-                require("zk").setup()
+                require("zk").setup({
+                    -- can be "telescope", "fzf" or "select" (`vim.ui.select`)
+                    -- it's recommended to use "telescope" or "fzf"
+                    picker = "telescope",
+
+                    lsp = {
+                        -- `config` is passed to `vim.lsp.start_client(config)`
+                        config = {
+                            cmd = { "zk", "lsp" },
+                            name = "zk",
+                            -- on_attach = ...
+                            -- etc, see `:h vim.lsp.start_client()`
+                        },
+
+                        -- automatically attach buffers in a zk notebook that match the given filetypes
+                        auto_attach = {
+                            enabled = true,
+                            filetypes = { "markdown" },
+                        },
+                    },
+                })
             end,
         })
         -- use({
@@ -194,14 +214,14 @@ function M.setup()
 
         -- Miscellaneous
         -- use("tpope/vim-repeat")
-        use({
-            "iamcco/markdown-preview.nvim",
-            run = "cd app && npm install",
-            setup = function()
-                vim.g.mkdp_filetypes = { "markdown" }
-            end,
-            ft = { "markdown" },
-        })
+        -- use({
+        --     "iamcco/markdown-preview.nvim",
+        --     run = "cd app && npm install",
+        --     setup = function()
+        --         vim.g.mkdp_filetypes = { "markdown" }
+        --     end,
+        --     ft = { "markdown" },
+        -- })
         use({
             "ellisonleao/glow.nvim",
             config = function()
@@ -350,7 +370,7 @@ function M.setup()
         })
 
         -- Syntax Plugin
-        use({'preservim/vim-markdown'})
+        use({ "preservim/vim-markdown" })
 
         -- Colorscheme
         use({
