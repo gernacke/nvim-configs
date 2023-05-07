d1d8cf9cc7b2fa387c04c5bc5036d5997f225d85
gernacke <gernacke@hotmail.com>
2023-05-07 16:14:25 +1000

    add source grep-operator at the end

--- init.lua
+++ init.lua
@@ -5,3 +5,7 @@ require("user.plugins").setup()
 require("user.lsp").setup()
 require("user.colorscheme")
 require("user.navic")
+
+vim.cmd [[
+    source $XDG_CONFIG_HOME/nvim/lua/user/myplugins/grep-operator.vim
+]]
