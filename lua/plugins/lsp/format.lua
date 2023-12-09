8f8715d6205efdacaa75d07bc29b1c938a715d8a
gernacke <gernacke@hotmail.com>
2023-12-09 17:10:37 +1100

    quick format

--- lua/plugins/lsp/format.lua
+++ lua/plugins/lsp/format.lua
@@ -3,8 +3,8 @@ local M = {}
 M.autoformat = false
 
 function M.toggle()
-	M.autoformat = not M.autoformat
-	vim.notify(M.autoformat and "Enabled format on save" or "Disabled format on save")
+  M.autoformat = not M.autoformat
+  vim.notify(M.autoformat and "Enabled format on save" or "Disabled format on save")
 end
 
 -- function M.format()
@@ -24,17 +24,17 @@ end
 -- end
 
 function M.on_attach(client, buf)
-	if client.supports_method("textDocument/formatting") then
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			group = vim.api.nvim_create_augroup("LspFormat." .. buf, {}),
-			buffer = buf,
-			callback = function()
-				if M.autoformat then
-					M.format()
-				end
-			end,
-		})
-	end
+  if client.supports_method("textDocument/formatting") then
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = vim.api.nvim_create_augroup("LspFormat." .. buf, {}),
+      buffer = buf,
+      callback = function()
+        if M.autoformat then
+          M.format()
+        end
+      end,
+    })
+  end
 end
 
 return M
