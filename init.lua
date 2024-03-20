cff13cb44d76c78286054660e2f6d938f25270cb
gernacke <gernacke@hotmail.com>
2024-03-20 17:57:53 +1100

    reformatting changes

--- init.lua
+++ init.lua
@@ -1,11 +1,11 @@
-require "config.options"
-require "config.lazy"
+require("config.options")
+require("config.lazy")
 
 vim.api.nvim_create_autocmd("User", {
   pattern = "VeryLazy",
   callback = function()
-    require "config.autocmds"
-    require "config.keymaps"
-    require "config.highlightgroup"
+    require("config.keymaps")
+    require("config.autocmds")
+    require("config.highlightgroup")
   end,
 })
