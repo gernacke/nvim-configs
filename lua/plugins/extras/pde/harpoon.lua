303671f5674a45ea26e341ba9e429e63872f6d2f
gernacke <gernacke@hotmail.com>
2024-03-22 11:50:37 +1100

    change: disable harpoon

--- lua/plugins/extras/pde/harpoon.lua
+++ lua/plugins/extras/pde/harpoon.lua
@@ -1,6 +1,7 @@
 return {
   {
     "ThePrimeagen/harpoon",
+    enabled = false,
     --stylua: ignore
     keys = {
       { "<leader>ja", function() require("harpoon.mark").add_file() end, desc = "Add File" },
