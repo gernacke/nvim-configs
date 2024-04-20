c8fd13ceb1cf669870755056a9107dee1061ada7
gernacke <43158395+gernacke@users.noreply.github.com>
2024-04-20 15:12:15 +1000

    disable Noice message notification UI

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,11 +1,14 @@
 local M = {
   "folke/noice.nvim",
   event = "VeryLazy",
-  enabled = false, -- disable for now
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim",
+  },
 }
 
 function M.config()
-  require("noice").setup {
+  require("noice").setup({
     lsp = {
       override = {
         ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
@@ -18,10 +21,23 @@ function M.config()
       command_palette = true,
       long_message_to_split = true,
       inc_rename = true,
-      lsp_doc_border = false,
+      lsp_doc_border = true,
       cmdline_output_to_split = false,
     },
-  }
+    messages = {
+      -- NOTE: If you enable messages, then the cmdline is enabled automatically.
+      -- This is a current Neovim limitation.
+      enabled = false, -- enables the Noice messages UI
+    },
+    views = {
+      cmdline_popup = {
+        position = {
+          row = "50%",
+          col = "50%",
+        },
+      },
+    },
+  })
 end
 
 return M
