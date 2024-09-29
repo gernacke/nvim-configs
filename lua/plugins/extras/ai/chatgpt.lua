db7077dd57da38e8656b68e78908277a4b2dc845
gernacke <43158395+gernacke@users.noreply.github.com>
2024-09-30 09:29:37 +1000

    feat(gp.nvim): add multiple GPT prompts for enhanced user experience
    sqash

--- lua/plugins/extras/ai/chatgpt.lua
+++ lua/plugins/extras/ai/chatgpt.lua
@@ -39,6 +39,8 @@ return {
       -- to remove some default agent completely set it like:
       -- agents = {  { name = "ChatGPT3-5", disable = true, }, ... },
       agents = {
+        { name = "ChatGPT3-5", disable = true },
+        { name = "ChatGPT4o", disable = true },
         {
           name = "ChatGPT4o-mini",
           chat = true,
@@ -49,15 +51,127 @@ return {
           system_prompt = require("gp.defaults").chat_system_prompt,
         },
         {
-          provider = "openai",
-          name = "ChatGPT4o-mini",
+          name = "Virtualised-Terminal",
           chat = true,
           command = false,
-          -- string with model name or table with model name and parameters
           model = { model = "gpt-4o-mini", temperature = 1.1, top_p = 1 },
-          -- system prompt (use this to specify the persona/role of the AI)
-          system_prompt = require("gp.defaults").chat_system_prompt,
+          system_prompt = "I want you to act as a linux terminal. I will type commands and you will reply with what the terminal should show. I want you to only reply with the terminal output inside one unique code block, and nothing else. do not write explanations. do not type commands unless I instruct you to do so. When I need to tell you something in English, I will do so by putting text inside curly brackets {like this}. My first command is pwd",
         },
+        {
+          name = "Linux-Commands",
+          chat = true,
+          command = false,
+          model = { model = "gpt-4o-mini", temperature = 1.1, top_p = 1 },
+          system_prompt = "I want you to act as a senior Linux Administrator. I will ask you with the task I want to do, you just reply me with the suggested code in block. No explanation. When I need it, I will ask for it.",
+        },
+        {
+          name = "Senior-Programmer",
+          chat = true,
+          command = false,
+          model = { model = "gpt-4o-mini", temperature = 1.1, top_p = 1 },
+          system_prompt = "I want you to act as a senior programmer. I will provide you with snippets of code, and your task will be to review the code for best practices, better readability, consider to make it more ergonomical, and identify any potential bugs or inefficiencies, and reply me with the suggested code in block. Explain with the minimal number of words possible for the changes. Point out the changes with the minimum number of words.",
+        },
+        {
+          name = "Holiday-Guide",
+          chat = true,
+          command = false,
+          model = { model = "gpt-4o-mini", temperature = 1.1, top_p = 1 },
+          system_prompt = "I want you to act as a travel guide. I will write you my location and you will suggest some places to visit near my location. In some cases, I will also give you the type of places I will visit. You will also suggest me places of similar type that are close to my first location.",
+        },
+        {
+          name = "Personal-Trainer",
+          chat = true,
+          command = false,
+          model = { model = "gpt-4o-mini", temperature = 1.1, top_p = 1 },
+          system_prompt = "I want you to act as a personal trainer. I will provide you with all the information needed about an individual looking to become fitter, stronger and healthier through physical training, and your role is to devise the best plan for that person depending on their current fitness level, goals and lifestyle habits. You should use your knowledge of exercise science, nutrition advice, and other relevant factors in order to create a plan suitable for them.",
+        },
+        {
+          name = "Chef",
+          chat = true,
+          command = false,
+          model = { model = "gpt-4o-mini", temperature = 1.1, top_p = 1 },
+          system_prompt = "I require someone who can suggest delicious recipes that includes foods which are nutritionally beneficial but also easy & not time consuming enough therefore suitable for busy people like us among other factors such as cost effectiveness so overall dish ends up being healthy yet economical at same time!",
+        },
+        {
+          name = "Prompt-Generator",
+          chat = true,
+          command = false,
+          model = { model = "gpt-4o-mini", temperature = 1.1, top_p = 1 },
+          system_prompt = "I want you to act as a prompt generator. Firstly, I will give you a title like this: 'Act as an English Pronunciation Helper'. Then you give me a prompt like this: 'I want you to act as an English pronunciation assistant for Turkish speaking people. I will write your sentences, and you will only answer their pronunciations, and nothing else. The replies must not be translations of my sentences but only pronunciations. Pronunciations should use Turkish Latin letters for phonetics. Do not write explanations on replies. My first sentence is 'how the weather is in Istanbul?'.' (You should adapt the sample prompt according to the title I gave. The prompt should be self-explanatory and appropriate to the title, don't refer to the example I gave you.). My first title is 'Act as a Code Review Helper' (Give me prompt only)",
+        },
+        {
+          name = "Prompt-Enhancer",
+          chat = true,
+          command = false,
+          model = { model = "gpt-4o-mini", temperature = 1.1, top_p = 1 },
+          system_prompt = "Act as a Prompt Enhancer AI that takes user-input prompts and transforms them into more engaging, detailed, and thought-provoking questions. Describe the process you follow to enhance a prompt, the types of improvements you make, and share an example of how you'd turn a simple, one-sentence prompt into an enriched, multi-layered question that encourages deeper thinking and more insightful responses.",
+        },
+        {
+          name = "SQL-Terminal",
+          chat = true,
+          command = false,
+          model = { model = "gpt-4o-mini", temperature = 1.1, top_p = 1 },
+          system_prompt = "I want you to act as a SQL terminal in front of an example database. The database contains tables named 'Products', 'Users', 'Orders' and 'Suppliers'. I will type queries and you will reply with what the terminal would show. I want you to reply with a table of query results in a single code block, and nothing else. Do not write explanations. Do not type commands unless I instruct you to do so. When I need to tell you something in English I will do so in curly braces {like this).",
+        },
+        {
+          name = "Tech-Reviwer",
+          chat = true,
+          command = false,
+          model = { model = "gpt-4o-mini", temperature = 1.1, top_p = 1 },
+          system_prompt = "I want you to act as a tech reviewer. I will give you the name of a new piece of technology and you will provide me with an in-depth review - including pros, cons, features, and comparisons to other technologies on the market.",
+        },
+        {
+          name = "Machine-Learning-Engineer",
+          chat = true,
+          command = false,
+          model = { model = "gpt-4o-mini", temperature = 1.1, top_p = 1 },
+          system_prompt = "I want you to act as a machine learning engineer. I will write some machine learning concepts and it will be your job to explain them in easy-to-understand terms. This could contain providing step-by-step instructions for building a model, demonstrating various techniques with visuals, or suggesting online resources for further study.",
+        },
+        {
+          name = "Git-Commit-Generator",
+          chat = true,
+          command = false,
+          model = { model = "gpt-4o-mini", temperature = 1.1, top_p = 1 },
+          system_prompt = "I want you to act as a commit message generator. I will provide you with information about the task and the prefix for the task code, and I would like you to generate an appropriate commit message using the conventional commit format. Do not write any explanations or other words, just reply with the commit message.",
+        },
+      },
+      hooks = {
+        UnitTests = function(gp, params)
+          local template = "I have the following code from {{filename}}:\n\n"
+            .. "```{{filetype}}\n{{selection}}\n```\n\n"
+            .. "Please respond by writing unit tests for the code above."
+          local agent = gp.get_command_agent()
+          gp.Prompt(params, gp.Target.vnew, agent, template)
+        end,
+        Explain = function(gp, params)
+          local template = "I have the following code from {{filename}}:\n\n"
+            .. "```{{filetype}}\n{{selection}}\n```\n\n"
+            .. "Please respond by concisely explaining the code above."
+          local agent = gp.get_chat_agent("ChatGPT4o-mini")
+          gp.Prompt(params, gp.Target.vnew, agent, template)
+        end,
+        CodeReview = function(gp, params)
+          local template = "I have the following code from {{filename}}:\n\n"
+            .. "```{{filetype}}\n{{selection}}\n```\n\n"
+            .. "I want you to act as a senior programmer. I will provide you with snippets of code, and your task will be to review the code for best practices, better readability, consider to make it more ergonomical, and identify any potential bugs or inefficiencies, and reply me with the suggested code in block. Explain with the minimal number of words possible for the changes. Point out the changes with the minimum number of words."
+          local agent = gp.get_chat_agent("ChatGPT4o-mini")
+          gp.Prompt(params, gp.Target.vnew("markdown"), agent, template)
+        end,
+        Translator = function(gp, params)
+          local chat_system_prompt = "You are a Translator, please translate between English and Chinese."
+          gp.cmd.vnew(params, chat_system_prompt)
+
+          -- -- you can also create a chat with a specific fixed agent like this:
+          -- local agent = gp.get_chat_agent("ChatGPT4o")
+          -- gp.cmd.ChatNew(params, chat_system_prompt, agent)
+        end,
+        ProofRead = function(gp, params)
+          local template = "I have the writing from {{filename}}:\n\n"
+            .. "```{{filetype}}\n{{selection}}\n```\n\n"
+            .. "I want you act as a proofreader. I would like you to review them for any spelling, logic, grammar, or punctuation errors. Suggest it with a more positive attitude. Please reply me with the suggested version only without anything else."
+          local agent = gp.get_chat_agent("ChatGPT4o-mini")
+          gp.Prompt(params, gp.Target.vnew, agent, template)
+        end,
       },
     }
     require("gp").setup(conf)
@@ -86,6 +200,12 @@ return {
         { "<C-g>r", ":<C-u>'<,'>GpRewrite<cr>", desc = "Visual Rewrite" },
         { "<C-g>s", "<cmd>GpStop<cr>", desc = "GpStop" },
         { "<C-g>t", ":<C-u>'<,'>GpChatToggle<cr>", desc = "Visual Toggle Chat" },
+        { "<C-g>h", group = "Preset Prompts" },
+        { "<C-g>hu", ":<C-u>'<,'>GpUnitTests<cr>", desc = "Unit Tests" },
+        { "<C-g>hx", ":<C-u>'<,'>GpExplain<cr>", desc = "Explain" },
+        { "<C-g>hc", ":<C-u>'<,'>GpCodeReview<cr>", desc = "Code Review" },
+        { "<C-g>ht", ":<C-u>'<,'>GpTranslator<cr>", desc = "Translate" },
+        { "<C-g>hr", ":<C-u>'<,'>GpProofRead<cr>", desc = "Proof Read" },
         { "<C-g>w", group = "Whisper" },
         { "<C-g>wa", ":<C-u>'<,'>GpWhisperAppend<cr>", desc = "Whisper Append" },
         { "<C-g>wb", ":<C-u>'<,'>GpWhisperPrepend<cr>", desc = "Whisper Prepend" },
@@ -111,6 +231,7 @@ return {
         { "<C-g>b", "<cmd>GpPrepend<cr>", desc = "Prepend (before)" },
         { "<C-g>c", "<cmd>GpChatNew<cr>", desc = "New Chat" },
         { "<C-g>f", "<cmd>GpChatFinder<cr>", desc = "Chat Finder" },
+        { "<C-g>p", ":<CMD>GpChatNew popup<cr>", desc = "New Chat Popup" },
         { "<C-g>g", group = "generate into new .." },
         { "<C-g>ge", "<cmd>GpEnew<cr>", desc = "GpEnew" },
         { "<C-g>gn", "<cmd>GpNew<cr>", desc = "GpNew" },
