3a1cde5dabf8e92bc9864c48d790c1e14559e0ad
gernacke <43158395+gernacke@users.noreply.github.com>
2024-10-02 09:25:52 +1000

    chore: remove chatgpt.nvim plugin configurations

--- lua/plugins/extras/ai/chatgpt.lua
+++ lua/plugins/extras/ai/chatgpt.lua
@@ -1,32 +1,5 @@
 -- local home = vim.fn.expand("$HOME")
 local wk = require("which-key")
--- return {
---   "jackMort/ChatGPT.nvim",
---   cmd = { "ChatGPT", "ChatGPTRun", "ChatGPTActAs", "ChatGPTCompleteCode", "ChatGPTEditWithInstructions" },
---   enabled = true,
---   opts = {
---     api_key_cmd = "gpg --decrypt " .. home .. "/repositories/all-dotfiles/chatgpt/api-key.gpg",
---     popup_layout = {
---       default = "center",
---       center = {
---         width = "65%",
---         height = "80%",
---       },
---       right = {
---         width = "30%",
---         width_settings_open = "50%",
---       },
---     },
---   },
---   dependencies = {
---     "MunifTanjim/nui.nvim",
---     "nvim-lua/plenary.nvim",
---     "nvim-telescope/telescope.nvim",
---   },
---   config = function(_, opts)
---     require("chatgpt").setup(opts)
---   end,
--- }
 return {
   "robitx/gp.nvim",
   lazy = false,
