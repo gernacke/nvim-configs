66c671cb58167878156863eb3c5e3c509d79dacc
gernacke <43158395+gernacke@users.noreply.github.com>
2025-03-18 10:23:37 +1100

    (markdown-preview)fixed and working

--- lua/plugins/extras/pde/notes/markdown.lua
+++ lua/plugins/extras/pde/notes/markdown.lua
@@ -24,10 +24,10 @@ return {
   },
   {
     "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
-    build = "cd app && npm install",
-    init = function()
-      vim.g.mkdp_filetypes = { "markdown" }
+    build = function()
+      vim.fn["mkdp#util#install"]()
     end,
   },
   {
