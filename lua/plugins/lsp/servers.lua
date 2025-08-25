20a59340ee864a80c56ab3900b924688e42b5e9b
gernacke <43158395+gernacke@users.noreply.github.com>
2025-08-25 11:42:28 +1000

    fix:Ignore Rust LSP Warnings

--- lua/plugins/lsp/servers.lua
+++ lua/plugins/lsp/servers.lua
@@ -14,6 +14,17 @@ local function lsp_init()
     vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = sign.name })
   end
 
+  -- Ignore Rust LSP Warnings: rust_analyzer: -32802: server cancelled the request
+  for _, method in ipairs({ "textDocument/diagnostic", "workspace/diagnostic" }) do
+    local default_diagnostic_handler = vim.lsp.handlers[method]
+    vim.lsp.handlers[method] = function(err, result, context, config)
+      if err ~= nil and err.code == -32802 then
+        return
+      end
+      return default_diagnostic_handler(err, result, context, config)
+    end
+  end
+
   -- LSP handlers configuration
   local config = {
     float = {
