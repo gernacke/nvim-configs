4b238eb8d47ad2aa6de8153d81a17802a0747a0a
gernacke <43158395+gernacke@users.noreply.github.com>
2025-08-29 19:47:05 +1000

    feat(autocmds): customize Copilot chat buffer UI settings on BufEnter

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -103,6 +103,15 @@ vim.api.nvim_create_autocmd("BufReadPost", {
   end,
 })
 
+-- Auto-command to customize chat buffer behavior
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "copilot-*",
+  callback = function()
+    vim.opt_local.relativenumber = false
+    vim.opt_local.number = false
+    vim.opt_local.conceallevel = 0
+  end,
+})
 -- show cursor line only in active window
 vim.api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
   callback = function()
